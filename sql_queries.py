58ca4b84272f57cd3606f78b02f3f1b174e7119b
Greg Williams <greggwilliams58@gmail.com>
2020-10-04 15:29:58 +0100

    documentation written up
    insert statements finalised

--- sql_queries.py
+++ sql_queries.py
@@ -33,10 +33,10 @@ staging_events_table_create = ("""CREATE TABLE IF NOT EXISTS staging_events (
         level              nvarchar(50),
         location           text,
         method             nvarchar(5),
-        page               nvarchar,
+        page               nvarchar(25),
         registration       float,
         session_id         int,
-        song               nvarchar(256),
+        song               text,
         status             int,
         ts                 bigint,
         user_agent         nvarchar(256),
@@ -84,7 +84,7 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS dimt_users  (
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS dimt_songs (
         song_id      nvarchar(25) NOT NULL PRIMARY KEY,
-        title        nvarchar(150),
+        title        text,
         artist_id    nvarchar(25),
         year         int,
         duration     numeric(11,5)
@@ -95,7 +95,7 @@ song_table_create = ("""CREATE TABLE IF NOT EXISTS dimt_songs (
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS dimt_artists (
         artist_id         nvarchar(25) NOT NULL PRIMARY KEY,
         artist_name       nvarchar(500),
-        artist_location   nvarchar(60),
+        artist_location   text,
         artist_latitude   numeric(20,8),
         artist_longitude  numeric(20,8) 
         )
@@ -109,7 +109,7 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS dimt_time (
         week        int,
         month       int,
         year        int,
-        weekday     boolean
+        weekday     int
         )
         diststyle all;
 """)
@@ -131,7 +131,7 @@ staging_songs_copy = ("""COPY staging_songs FROM {}
 
 # FINAL TABLES
 songplay_table_insert = ("""INSERT INTO                                   fact_songplays(start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
-SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, user_id ,level,song_id,artist_id,session_id,location,user_agent
+SELECT distinct TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, user_id ,level,song_id,artist_id,session_id,location,user_agent
 FROM staging_events AS events
 JOIN staging_songs AS songs
 ON (events.artist = songs.artist_name)
@@ -141,18 +141,32 @@ WHERE page = 'NextSong'
                              
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO dimt_users (user_id,user_first_name,user_last_name,gender,level)
+                        SELECT distinct user_id ,first_name,last_name,gender,level
+                        FROM staging_events WHERE user_id IS NOT NULL
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO dimt_songs(song_id,title,artist_id,year, duration)
+                        SELECT distinct song_id, title, artist_id, year,duration
+                        FROM staging_songs
 """)
 
 artist_table_insert = ("""INSERT INTO dimt_artists (artist_id, artist_name,artist_location,artist_latitude,artist_longitude)
-                           SELECT DISTINCT (artist_id,artist_name,artist_location,artist_latitude ,artist_longitude)
+                           SELECT DISTINCT artist_id,artist_name,artist_location,artist_latitude ,artist_longitude
                            FROM staging_songs WHERE artist_id IS NOT NULL
 """)
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO dimt_time (start_time,hour,week, month,year,weekday)
+                     (
+                     SELECT T.start_time,
+                     EXTRACT (HOUR FROM T.start_time),
+                     EXTRACT (WEEK FROM T.start_time),
+                     EXTRACT (MONTH FROM T.start_time),
+                     EXTRACT (YEAR FROM T.start_time),
+                     EXTRACT (WEEKDAY FROM T.start_time)                     
+                     FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' as start_time FROM staging_events)
+                         AS T
+                         )
 """)
 
 # QUERY LISTS
@@ -163,5 +177,4 @@ drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songp
 
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 
-#insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-insert_table_queries = [songplay_table_insert]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
